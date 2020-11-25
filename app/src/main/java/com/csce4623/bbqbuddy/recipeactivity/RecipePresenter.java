b2dac7ebffa40c4a23796925dc0992b0ee4113b9
Hawk Dykes <hwdykes@uark.edu>
2020-11-24 20:32:07 -0600

    recipe and session activities & layouts

--- app/src/main/java/com/csce4623/bbqbuddy/recipeactivity/RecipePresenter.java
+++ app/src/main/java/com/csce4623/bbqbuddy/recipeactivity/RecipePresenter.java
@@ -1,4 +1,31 @@
 package com.csce4623.bbqbuddy.recipeactivity;
 
-public class RecipePresenter {
+import androidx.annotation.NonNull;
+
+import com.csce4623.bbqbuddy.data.Repository;
+
+public class RecipePresenter implements RecipeContract.Presenter{
+
+    //Data repository instance
+    //Currently has a memory leak -- need to refactor context passing
+    private static Repository mRepository;
+    //View instance
+    private final RecipeContract.View mView;
+
+    /**
+     * ToDoListPresenter constructor
+     * @param itemRepository - Data repository instance
+     * @param itemView - MainContract.View instance
+     */
+    public RecipePresenter(@NonNull Repository itemRepository, @NonNull RecipeContract.View itemView){
+        mRepository = itemRepository;
+        mView = itemView;
+        //Make sure to pass the presenter into the view!
+        mView.setPresenter(this);
+    }
+
+    @Override
+    public void start(){
+    }
+
 }

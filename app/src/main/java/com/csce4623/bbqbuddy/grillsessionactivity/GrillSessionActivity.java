bbe49217b8f1cc6b29a0d2c181e3a631194303f5
Hawk Dykes <hwdykes@uark.edu>
2020-11-25 10:28:36 -0600

    recipe and session activities & layouts

--- app/src/main/java/com/csce4623/bbqbuddy/grillsessionactivity/GrillSessionActivity.java
+++ app/src/main/java/com/csce4623/bbqbuddy/grillsessionactivity/GrillSessionActivity.java
@@ -1,7 +1,10 @@
 package com.csce4623.bbqbuddy.grillsessionactivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,14 +17,48 @@ public class GrillSessionActivity extends AppCompatActivity implements GrillSess
 
     private GrillSessionContract.Presenter mPresenter;
 
+    LinearLayout LLMeatSelect, LLFollowRecipe, LLSetTimer;
+    Button btnStartSession;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.recipe_layout);
+        setContentView(R.layout.session_config_layout);
         ImageView recipeImage = (ImageView) findViewById(R.id.imageView);
+        LLMeatSelect = (LinearLayout) findViewById(R.id.LLMeatSelectBar);
+        LLFollowRecipe = (LinearLayout) findViewById(R.id.LLFollowRecipeBar);
+        LLSetTimer = (LinearLayout) findViewById(R.id.LLSetTimerBar);
+        btnStartSession = (Button) findViewById(R.id.btnStartSession);
 
         mPresenter = new GrillSessionPresenter(Repository.getInstance(new AppExecutors(),getApplicationContext()), this);
 
+        LLMeatSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // go to activity/fragment for selecting meat for grill session
+            }
+        });
+
+        LLFollowRecipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // go to activity/fragment for selecting a recipe to follow
+            }
+        });
+
+        LLSetTimer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // go to activity/fragment for setting timer(s)
+            }
+        });
+
+        btnStartSession.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // launch grill session
+            }
+        });
     }
 
     @Override

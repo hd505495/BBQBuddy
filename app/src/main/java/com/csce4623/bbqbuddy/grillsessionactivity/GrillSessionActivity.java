c7a3beb63fbdd25fd99504b29526ac031caf30af
Hawk Dykes <hwdykes@uark.edu>
2020-11-25 10:13:52 -0600

    recipe and session activities & layouts

--- app/src/main/java/com/csce4623/bbqbuddy/grillsessionactivity/GrillSessionActivity.java
+++ app/src/main/java/com/csce4623/bbqbuddy/grillsessionactivity/GrillSessionActivity.java
@@ -1,4 +1,43 @@
 package com.csce4623.bbqbuddy.grillsessionactivity;
 
-public class GrillSessionActivity {
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.csce4623.bbqbuddy.R;
+import com.csce4623.bbqbuddy.data.Repository;
+
+import util.AppExecutors;
+
+public class GrillSessionActivity extends AppCompatActivity implements GrillSessionContract.View {
+
+    private GrillSessionContract.Presenter mPresenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recipe_layout);
+        ImageView recipeImage = (ImageView) findViewById(R.id.imageView);
+
+        mPresenter = new GrillSessionPresenter(Repository.getInstance(new AppExecutors(),getApplicationContext()), this);
+
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        mPresenter.start();
+    }
+
+
+    /**
+     * set the presenter for this view
+     * @param presenter - the GrillSessionContract.presenter instance
+     */
+    @Override
+    public void setPresenter(GrillSessionContract.Presenter presenter) {
+        mPresenter = presenter;
+    }
+
 }
--- app/src/main/java/com/csce4623/bbqbuddy/grillsessionactivity/GrillSessionPresenter.java
+++ app/src/main/java/com/csce4623/bbqbuddy/grillsessionactivity/GrillSessionPresenter.java
@@ -1,4 +1,30 @@
 package com.csce4623.bbqbuddy.grillsessionactivity;
 
-public class GrillSessionPresenter {
+import androidx.annotation.NonNull;
+
+import com.csce4623.bbqbuddy.data.Repository;
+
+public class GrillSessionPresenter implements GrillSessionContract.Presenter{
+
+    //Data repository instance
+    //Currently has a memory leak -- need to refactor context passing
+    private static Repository mRepository;
+    //View instance
+    private final GrillSessionContract.View mView;
+
+    /**
+     * ToDoListPresenter constructor
+     * @param itemRepository - Data repository instance
+     * @param itemView - GrillSessionContract.View instance
+     */
+    public GrillSessionPresenter(@NonNull Repository itemRepository, @NonNull GrillSessionContract.View itemView){
+        mRepository = itemRepository;
+        mView = itemView;
+        //Make sure to pass the presenter into the view!
+        mView.setPresenter(this);
+    }
+
+    @Override
+    public void start() {
+    }
 }
